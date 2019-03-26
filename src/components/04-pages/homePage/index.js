d20da550f16146181ded49a28aff27362ad135a1
bau123 <bauvannguyen123@gmail.com>
2019-03-26 13:18:48 +0000

    Added Organism: Navigation Bar Template: Default Page Pages: Home Page

---
+++ src/components/04-pages/homePage/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Page from '../../03-templates/defaultPage/';
+import Body from './body/index';
+
+
+const HomePage = () => {
+    return (
+        <Page>
+            <Body/>
+        </Page>
+    )
+};
+
+export default HomePage;
